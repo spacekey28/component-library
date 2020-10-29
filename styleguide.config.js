d4b401c63fa9b1743fac2b9b94591fc4c5d82d1b
Peter Son <peteson.me@gmail.com>
2020-10-29 23:19:09 +1300

    build documentation

--- styleguide.config.js
+++ styleguide.config.js
@@ -1,13 +1,20 @@
+const path = require("path");
+
 module.exports = {
-    webpackConfig: {
-      module: {
-        rules: [
-          {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: "babel-loader"
-          }
-        ]
-      }
-    },
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: "babel-loader"
+        }
+      ]
+    }
+  },
+  title: "React Components Library",
+  styleguideDir: "dist-docs",
+  moduleAliases: {
+    "react-components-library": path.resolve(__dirname, "src")
+  }
 };
